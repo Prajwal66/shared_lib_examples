fc16d3c497518672c357293090049fc57baa6573
Prajwal66 <74700045+Prajwal66@users.noreply.github.com>
2025-07-08 15:35:53 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,5 @@
 def clone(String url, String branch)
 {
   echo " this is cloning the code"
-  git url: "https://github.com/Prajwal66/shared_lib_examples.git", branch: "main"
+  git url: "${url}", branch: ${branch}
 }
